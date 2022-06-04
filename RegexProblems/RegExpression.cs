8226dcf20f658c15fffddfad7baa88db4f659e84
[prasanna] <prasanna>
2022-06-04 22:54:11 +0530

    [prasanna]; check mobile number is valid or not

--- RegexProblems/RegExpression.cs
+++ RegexProblems/RegExpression.cs
@@ -12,6 +12,7 @@ namespace RegexProblems
         const string FIRSTNAME = "^[A-Z]{1}[a-z]{2,}$";
         const string LASTNAME = "^[A-Z]{1}[a-z]{2,}$";
         const string EMAIL = "^(abc).?[a-z]{3,}[@](bl).?(co).?[a-z]{2,}$";
+        const string CONTACT = "^[0-9]{1,3}[ ][789][0-9]{9}$";
         public void RegexFirstNameChecker(string firstName)
         {
             if (Regex.IsMatch(firstName, FIRSTNAME))
@@ -38,6 +39,15 @@ namespace RegexProblems
                 return;
             }
             Console.WriteLine("E-Mail id is not valid.");
+        }
+        public void RegexMobileNumber(string mobile)
+        {
+            if (Regex.IsMatch(mobile, CONTACT))
+            {
+                Console.WriteLine("Mobile number is valid.");
+                return;
+            }
+            Console.WriteLine("Mobile number is not valid.");
         }
     }
 }
